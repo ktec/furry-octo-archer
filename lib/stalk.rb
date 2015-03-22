dc1ac2937bb93987860d26a366ce5ee549e629c4
Keith Salisbury <keithsalisbury@gmail.com>
2015-03-22 16:47:31 +0000

    more scraper sites...

--- lib/stalk.rb
+++ lib/stalk.rb
@@ -7,21 +7,15 @@ Domain::Github::Processor.new("https://github.com/search?o=desc&q=ruby&s=reposit
 
 # We can then search stackoverflow for the username, if we find it
 # we can compare some stats and potentially build up another aspect
-# of the user. http://stackoverflow.com/users?tab=Reputation&filter=all&search=rubysolo
-
-# Then we've got google plus..
-# https://plus.google.com/s/rubysolo
-
-# Reddit
-# http://www.reddit.com/search?q=author%3Arubysolo&
-
-# Twitter...of course!
-# https://twitter.com/rubysolo
-
-# Instagram...
-# https://instagram.com/rubysolo/
+# of the user.
 
 # Oh my, this is turning into a serious stalker database...haha!
-
-# Why not, what about disqus
-# https://disqus.com/by/Zepalesque/
+# http://stackoverflow.com/users?tab=Reputation&filter=all&search=#{:username}
+# https://plus.google.com/s/#{:username}
+# http://www.reddit.com/search?q=author%3A#{:username}&
+# https://twitter.com/#{:username}
+# https://instagram.com/#{:username}/
+# https://disqus.com/by/#{:username}/
+# http://codepen.io/#{:username}/
+# https://keybase.io/#{:username}
+# https://www.linkedin.com/in/#{:username} <--- lots of "skills" here
